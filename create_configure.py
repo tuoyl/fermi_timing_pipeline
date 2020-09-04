dac6bb001c19de65ad7bb9c5a26ec1f82cd21ff5
tuoyl <youlituo@gmail.com>
2020-09-04 22:50:13 +0800

    python script to create yaml file

---
+++ create_configure.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import yaml
+import sys
+
+def get_parlist(template):
+    with open(template)as fin:
+        parlist = yaml.load(fin, Loader=yaml.FullLoader)
+    return parlist
+
+def change_configure(write_yamlfile, dictionary, week_num):
+    for datakey in dictionary['data']:
+        print(datakey, dictionary['data'][datakey])
+        dictionary['data'][datakey] = dictionary['data'][datakey].replace("???", str(int(week_num)))
+        print(dictionary['data'][datakey])
+
+    with open(write_yamlfile, 'w') as fout:
+        documents = yaml.dump(dictionary, fout)
+
+
+if __name__ == "__main__":
+    template = sys.argv[1]
+    newyaml  = sys.argv[2]
+    week_num = sys.argv[3]
+
+    ## read yaml template
+    dictionary = get_parlist(template)
+
+    ## change the key word
+    change_configure(newyaml, dictionary, week_num)
+
